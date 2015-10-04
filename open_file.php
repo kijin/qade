cfb7fd9d71b9a193852bed5ffd101a9dc1da386c
Kijin Sung <kijin@kijinsung.com>
2015-10-04 13:03:56 +0900

    Use JSON options when opening a file

--- open_file.php
+++ open_file.php
@@ -12,4 +12,4 @@ $filename = isset($_GET['file']) ? trim($_GET['file'], '/') : '';
 $filename = str_replace('..', '', $filename);
 $filename = $config['basedir'] . '/' . $filename;
 
-echo json_encode(get_file_info($filename));
+echo json_encode(get_file_info($filename), JSON_HEX_QUOT | JSON_HEX_TAG | JSON_HEX_AMP);
