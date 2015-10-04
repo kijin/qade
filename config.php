60769a2f785d87f88111eaae3d15231ae281ec09
Kijin Sung <kijin@kijinsung.com>
2015-10-04 14:14:00 +0900

    Allow user to override default config

--- config.php
+++ config.php
@@ -4,3 +4,8 @@ $config['basedir'] = dirname(__DIR__);
 $config['detect_encodings'] = 'UTF-8';
 $config['default_encoding'] = 'UTF-8';
 $config['default_timezone'] = 'Etc/UTC';
+
+if (file_exists(__DIR__ . '/scratch/config.php'))
+{
+    include __DIR__ . '/scratch/config.php';
+}
